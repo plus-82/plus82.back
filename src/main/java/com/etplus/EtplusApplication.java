17ad996517fe253bc0f28ca2557f7fc7a0bb4277
Huey-J <wkdgus7113@gmail.com>
2024-07-11 17:52:20 +0900

    initial commit

---
+++ src/main/java/com/etplus/EtplusApplication.java
@@ -0,0 +1,13 @@
+package com.etplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtplusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/etplus/EtplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
