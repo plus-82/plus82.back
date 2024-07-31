c2cf5edc62fed72880bfb1b34ce879364f8ce3cd
Huey-J <wkdgus7113@gmail.com>
2024-08-01 01:47:01 +0900

    feat: 구직자 회원가입 API 비즈니스 로직 구현

--- src/main/java/com/etplus/controller/dto/SignUpDto.java
+++ src/main/java/com/etplus/controller/dto/SignUpDto.java
@@ -1,19 +1,27 @@
 package com.etplus.controller.dto;
 
-import com.etplus.repository.domain.code.RoleType;
+import com.etplus.repository.domain.code.GenderType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import java.time.LocalDate;
 
 public record SignUpDto(
-    @NotBlank
-    String nickName,
+    // 계정 정보
     @NotBlank @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "Invalid email format")
-    String email,
+    String email,       // 아이디
     @NotBlank
-    String password,
+    String password,    // 비밀번호
+
+    // 사용자 정보
+    @NotBlank
+    String name,            // 이름
+    @NotBlank
+    String country,         // 국가
     @NotNull
-    RoleType roleType
+    GenderType genderType,  // 성별
+    @NotNull
+    LocalDate birthDate     // 생년월일
 ) {
 
 }
