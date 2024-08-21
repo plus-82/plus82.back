7439e4f0477d901fec30a653bf712d74b883d657
Huey-J <wkdgus7113@gmail.com>
2024-08-21 19:15:29 +0900

    Revert "feat: 이메일 인증 API 추가"
    This reverts commit f2b58692dd7637bb197b626cf341506fb0d1bc4d.

--- src/main/java/com/etplus/repository/EmailRepository.java
+++ src/main/java/com/etplus/repository/EmailRepository.java
@@ -1,11 +1,8 @@
 package com.etplus.repository;
 
 import com.etplus.repository.domain.EmailEntity;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmailRepository extends JpaRepository<EmailEntity, Long> {
-
-  Optional<EmailEntity> findByCode(String code);
 
 }
