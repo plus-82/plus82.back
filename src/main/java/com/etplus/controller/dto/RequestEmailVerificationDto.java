964e1210fbc8c9816b00d2cff929e218ff9aa4b1
Huey-J <wkdgus7113@gmail.com>
2024-09-03 23:49:32 +0900

    feat: RequestEmailVerificationDto 형식 검사 validation 추가

--- src/main/java/com/etplus/controller/dto/RequestEmailVerificationDto.java
+++ src/main/java/com/etplus/controller/dto/RequestEmailVerificationDto.java
@@ -1,9 +1,11 @@
 package com.etplus.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record RequestEmailVerificationDto(
     @NotBlank
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "Invalid email format")
     String email
 ) {
 
