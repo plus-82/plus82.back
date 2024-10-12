44696ff61686c5be3de587cc5b0beaf79b8e65f5
Huey-J <wkdgus7113@gmail.com>
2024-10-13 00:10:07 +0900

    feat: AcademyEntity 사업자등록번호 필드 추가

--- src/main/java/com/etplus/repository/domain/AcademyEntity.java
+++ src/main/java/com/etplus/repository/domain/AcademyEntity.java
@@ -28,6 +28,8 @@ public class AcademyEntity {
   private String name;
   @Column(length = 1000)
   private String description;
+  @Column(nullable = false, unique = true, length = 20)
+  private String businessRegistrationNumber;  // 사업자등록번호
 
   @Enumerated(EnumType.STRING)
   private LocationType locationType;
@@ -42,13 +44,13 @@ public class AcademyEntity {
 
   // todo image list?
 
-  public AcademyEntity(Long id, String name, String description, LocationType locationType,
-      String detailedAddress, boolean forKindergarten, boolean forElementary,
-      boolean forMiddleSchool,
-      boolean forHighSchool, boolean forAdult) {
+  public AcademyEntity(Long id, String name, String description, String businessRegistrationNumber,
+      LocationType locationType, String detailedAddress, boolean forKindergarten,
+      boolean forElementary, boolean forMiddleSchool, boolean forHighSchool, boolean forAdult) {
     this.id = id;
     this.name = name;
     this.description = description;
+    this.businessRegistrationNumber = businessRegistrationNumber;
     this.locationType = locationType;
     this.detailedAddress = detailedAddress;
     this.forKindergarten = forKindergarten;
