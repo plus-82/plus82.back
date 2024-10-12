eb4cf1a980482423017e35b11ba10455b894cedf
Huey-J <wkdgus7113@gmail.com>
2024-10-12 23:47:32 +0900

    feat: 학원 정보 테이블 추가

---
+++ src/main/java/com/etplus/repository/domain/AcademyEntity.java
@@ -0,0 +1,60 @@
+package com.etplus.repository.domain;
+
+import com.etplus.repository.domain.code.LocationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter @Setter
+@Entity
+@Table(name = "academy_entity")
+public class AcademyEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+  @Column(length = 1000)
+  private String description;
+
+  @Enumerated(EnumType.STRING)
+  private LocationType locationType;
+  private String detailedAddress;
+
+  // 대상
+  private boolean forKindergarten;
+  private boolean forElementary;
+  private boolean forMiddleSchool;
+  private boolean forHighSchool;
+  private boolean forAdult;
+
+  // todo image list?
+
+  public AcademyEntity(Long id, String name, String description, LocationType locationType,
+      String detailedAddress, boolean forKindergarten, boolean forElementary,
+      boolean forMiddleSchool,
+      boolean forHighSchool, boolean forAdult) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.locationType = locationType;
+    this.detailedAddress = detailedAddress;
+    this.forKindergarten = forKindergarten;
+    this.forElementary = forElementary;
+    this.forMiddleSchool = forMiddleSchool;
+    this.forHighSchool = forHighSchool;
+    this.forAdult = forAdult;
+  }
+}
