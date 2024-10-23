3c451f916895ed96cadb663b8ce4f5116bc2fe67
Huey-J <wkdgus7113@gmail.com>
2024-10-24 01:23:14 +0900

    fix typo

--- src/main/java/com/etplus/controller/CountryController.java
+++ src/main/java/com/etplus/controller/CountryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/counties")
+@RequestMapping("/api/v1/countries")
 public class CountryController {
 
   private final CountryService countryService;
