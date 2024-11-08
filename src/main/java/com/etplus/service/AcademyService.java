9a040ccbab5f4b98145faf856aacfa2838cb1c7d
Huey-J <wkdgus7113@gmail.com>
2024-11-08 17:37:29 +0900

    PD-117 이미지 조회 로직 수정

--- src/main/java/com/etplus/service/AcademyService.java
+++ src/main/java/com/etplus/service/AcademyService.java
@@ -8,6 +8,7 @@ import com.etplus.exception.ResourceNotFoundException;
 import com.etplus.exception.ResourceNotFoundException.ResourceNotFoundExceptionCode;
 import com.etplus.provider.S3ImageUploader;
 import com.etplus.repository.AcademyRepository;
+import com.etplus.repository.ImageFileRepository;
 import com.etplus.repository.UserRepository;
 import com.etplus.repository.domain.AcademyEntity;
 import com.etplus.repository.domain.ImageFileEntity;
@@ -28,6 +29,7 @@ public class AcademyService {
   private final AcademyRepository academyRepository;
   private final UserRepository userRepository;
   private final S3ImageUploader s3ImageUploader;
+  private final ImageFileRepository imageFileRepository;
 
   public AcademyDetailVO getMyAcademy(LoginUser loginUser) {
     UserEntity user = userRepository.findById(loginUser.userId())
@@ -39,6 +41,11 @@ public class AcademyService {
     if (!RoleType.ACADEMY.equals(user.getRoleType()) || academy == null) {
       throw new ResourceDeniedException(ResourceDeniedExceptionCode.INVALID_ROLE);
     }
+
+    List<Long> imageFileIdList = academy.getImageFileIdList();
+    List<ImageFileEntity> imageFileList = imageFileRepository.findAllByIdIn(imageFileIdList);
+
+    List<String> imagePathList = imageFileList.stream().map(ImageFileEntity::getPath).toList();
 
     return new AcademyDetailVO(
         academy.getId(),
@@ -52,7 +59,7 @@ public class AcademyService {
         academy.isForMiddleSchool(),
         academy.isForHighSchool(),
         academy.isForAdult(),
-        null  // TODO
+        imagePathList
     );
   }
 
