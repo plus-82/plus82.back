34a0d17d2f455049917655c40c5b3edc3c9eba7e
Huey-J <wkdgus7113@gmail.com>
2024-12-10 23:54:40 +0900

    PD-130 JobPostResumeRelationEntity 추가

---
+++ src/main/java/com/etplus/repository/JobPostResumeRelationRepository.java
@@ -0,0 +1,10 @@
+package com.etplus.repository;
+
+import com.etplus.repository.domain.JobPostResumeRelationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobPostResumeRelationRepository extends JpaRepository<JobPostResumeRelationEntity, Long> {
+
+}
---
+++ src/main/java/com/etplus/repository/domain/JobPostResumeRelationEntity.java
@@ -0,0 +1,53 @@
+package com.etplus.repository.domain;
+
+import com.etplus.repository.domain.code.JobPostResumeRelationStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Entity
+@Table(name = "job_post_resume_relation")
+public class JobPostResumeRelationEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private JobPostResumeRelationStatus status;
+  @Column(nullable = false)
+  private LocalDate submittedDate;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false, updatable = false)
+  private UserEntity user;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "job_post_id", referencedColumnName = "id", nullable = false, updatable = false)
+  private JobPostEntity jobPost;
+
+  public JobPostResumeRelationEntity(Long id, JobPostResumeRelationStatus status,
+      LocalDate submittedDate, UserEntity user, JobPostEntity jobPost) {
+    this.id = id;
+    this.status = status;
+    this.submittedDate = submittedDate;
+    this.user = user;
+    this.jobPost = jobPost;
+  }
+}
