1ce0ec2cd32b80475be534aec05936aa457200c1
Huey-J <wkdgus7113@gmail.com>
2025-09-28 15:45:23 +0900

    로그 설정 변경

--- src/main/java/com/etplus/controller/HealthCheckController.java
+++ src/main/java/com/etplus/controller/HealthCheckController.java
@@ -1,11 +1,13 @@
 package com.etplus.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/health-check")
@@ -14,6 +16,16 @@ public class HealthCheckController {
   @GetMapping
   public ResponseEntity<String> healthCheck() {
     return ResponseEntity.ok("OK");
+  }
+
+  @GetMapping("/logs")
+  public ResponseEntity<String> insertLogs() {
+    log.trace("[APP_LOG] trace log");
+    log.debug("[APP_LOG] debug log");
+    log.info("[APP_LOG] info log");
+    log.warn("[APP_LOG] warn log");
+    log.error("[APP_LOG] error log");
+    return ResponseEntity.ok("Log Inserted");
   }
 
 }
